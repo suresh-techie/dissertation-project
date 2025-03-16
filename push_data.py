6632e5ea11a27f8c3a32f5285ae21d084142d64e
Suresh V S <vssureshvs1995@gmail.com>
2025-03-16 19:12:57 +0530

    test case added

--- push_data.py
+++ push_data.py
@@ -1,5 +1,72 @@
 import requests
 import json
+import csv
+import re
+import os
+import nltk
+nltk.download('stopwords', quiet=True)
+nltk.download('punkt_tab', quiet=True)
+import pickle
+
+log_dir = r"output.log"
+failure_list = []
+
+summary = {"category": "", "count": 0, "testname": ""}
+error_list = ['RuntimeError', 'APIException', 'ScriptIssue', 'TimeoutError', 'OtherIssue', 'SetupIssue', 'ModemIssue', 'NetworkIssue']
+
+
+
+# Function to preprocess the log lines (tokenization, stop word removal)
+def preprocess_log(logs):
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    cleaned_logs = []
+    for log in logs:
+        tokens = nltk.word_tokenize(log)
+        tokens = [word.lower() for word in tokens if word.isalnum()]  # Remove punctuation
+        filtered_tokens = [word for word in tokens if word not in stop_words]  # Remove stop words
+        cleaned_logs.append(" ".join(filtered_tokens))
+    return cleaned_logs
+
+
+def predict_category(err_summary):
+    # Load saved components
+    with open("rf_model.pkl", "rb") as model_file:
+        rf_model = pickle.load(model_file)
+    with open("vectorizer.pkl", "rb") as vectorizer_file:
+        vectorizer = pickle.load(vectorizer_file)
+    with open("label_encoder.pkl", "rb") as label_encoder_file:
+        label_encoder = pickle.load(label_encoder_file)
+   
+    processed_log = preprocess_log(err_summary)
+    log_vector = vectorizer.transform(processed_log).toarray()
+    
+    # Predict and decode the label
+    prediction = rf_model.predict(log_vector)
+    predicted_label = label_encoder.inverse_transform(prediction)
+    return predicted_label[0]
+
+# Function to parse log files
+def parse_log_files(log_dir, error_keywords=["error", "exception", "failed"]):
+    with open(log_dir, "r") as f:
+        content = f.readlines()
+    for line in content:
+        if "FAILED" in line and "Exception:" in line:
+            for item in error_list:
+                if item in line:
+                    err_log = line
+                    break
+    return err_log
+
+
+parsed_log = parse_log_files(log_dir)
+test_name = re.findall(r"::(test.*)\s-", parsed_log)[0]
+err_summary = re.findall(r"Exception:(.*)", parsed_log)[0]
+failure_list.append(err_summary)
+category = predict_category(failure_list)
+
+summary["category"] = category
+summary["testname"] = test_name
+summary["count"] += 1
 
 # Define the API URL
 url = "http://127.0.0.1:5000/api/update_failures"
@@ -7,17 +74,17 @@ url = "http://127.0.0.1:5000/api/update_failures"
 # Define the request headers
 headers = {"Content-Type": "application/json"}
 
+
 # Define the request payload (JSON data)
 payload = {
     "failure_data": {
-        "failures": [
-            {"category": "Build Failure", "count": 10, "testname": "Test A"},
-            {"category": "Test Error", "count": 5, "testname": "Test B"},
-            {"category": "Warning", "count": 3, "testname": "Test C"},
-            {"category": "Dependency Issue", "count": 9}
-        ]
+        "failures": []
     }
 }
+
+payload["failure_data"]["failures"].append(summary)
+
+print(payload)
 
 # Convert dictionary to JSON string
 json_payload = json.dumps(payload)
@@ -25,6 +92,6 @@ json_payload = json.dumps(payload)
 # Send the POST request
 response = requests.post(url, headers=headers, data=json_payload)
 
-# Print response details
+# # Print response details
 print(f"Status Code: {response.status_code}")
 print(f"Response: {response.text}")
--- test1.py
+++ test1.py
@@ -9,27 +9,19 @@ from selenium.common.exceptions import NoSuchElementException
 import time
 import logging
 
-# text_box = "ta1"
-# text_box = "ta"
-
-# driver = webdriver.Chrome()
-# driver.get("https://omayo.blogspot.com/")
-# driver.maximize_window()
-# # driver.implicitly_wait(10)
-# import time
-# time.sleep(10)
-# driver.find_element(By.ID, text_box).send_keys("testing..")
-# time.sleep(10)
-
 import os
 print(os.environ['BUILD_NUMBER'])
 
 def test1_open_browser_url():
+    print("TEST NAME: test1_open_browser_url")
     text_box = "ta"
     driver = webdriver.Chrome()
     driver.get("https://omayo.blogspot.com/")
     driver.maximize_window()
-    # driver.implicitly_wait(10)
     time.sleep(10)
-    driver.find_element(By.ID, text_box).send_keys("This is for testing..")
+    try:
+        element = driver.find_element(By.ID, text_box)
+        element.send_keys("This is for testing..")
+    except Exception as err:
+        raise Exception("RuntimeError no such element Unable to locate element")
     time.sleep(10)
